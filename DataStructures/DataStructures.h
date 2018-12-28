81bf766eaeec37b23c2fa6ca133e4fd88b82ab99
Alexis Ortega <alexis.oc3@gmail.com>
2018-12-28 15:34:15 -0800

    Add framework files

---
+++ DataStructures/DataStructures.h
@@ -0,0 +1,19 @@
+//
+//  DataStructures.h
+//  DataStructures
+//
+//  Created by Alexis Ortega on 12/28/18.
+//  Copyright © 2018 Alexis Ortega. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DataStructures.
+FOUNDATION_EXPORT double DataStructuresVersionNumber;
+
+//! Project version string for DataStructures.
+FOUNDATION_EXPORT const unsigned char DataStructuresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataStructures/PublicHeader.h>
+
+
